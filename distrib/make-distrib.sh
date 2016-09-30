d2c4ac44af9f73efaf60fc60d0e891f732ea6f69
Hadrien Nilsson <github@psydk.org>
2016-09-30 22:38:33 +0200

    deb support

--- distrib/make-distrib.sh
+++ distrib/make-distrib.sh
@@ -3,13 +3,9 @@ POVER=2.5
 
 # Set current directory to the one containing this script
 cd "$(dirname "$0")"
-
 mkdir -p pngoptimizer-$POVER
 cd pngoptimizer-$POVER || HandleFail
 echo $PWD
-mkdir -p linux/pngoptimizercl
-
-make -C ../../projects/pngoptimizercl/ CONFIG=release
 
 function HandleFail
 {
@@ -33,15 +29,48 @@ function HandleSuccess
 
 ###############################################################################
 # PngOptimizer
+echo "========== Packaging PngOptimizer =========="
+make -C ../../projects/pngoptimizer/ CONFIG=release
 
-cp -v "../../projects/pngoptimizer/linux-release/pngoptimizer"  "linux/pngoptimizer/" || HandleFail
-cp -v "../../projects/pngoptimizer/Readme.txt"                  "linux/pngoptimizer/" || HandleFail
-cp -v "../../projects/pngoptimizer/License.txt"                 "linux/pngoptimizer/" || HandleFail
-cp -v "../../projects/pngoptimizer/Changelog.txt"               "linux/pngoptimizer/" || HandleFail
+POSRC=../../projects/pngoptimizer
+PODEB=deb/pngoptimizer_$POVER-1_amd64
+rm -rf $PODEB
+mkdir -p $PODEB
+mkdir -p $PODEB/DEBIAN
+mkdir -p $PODEB/usr/bin
+mkdir -p $PODEB/usr/share/icons/hicolor/128x128/apps
+mkdir -p $PODEB/usr/share/applications
+
+cp -v "$POSRC/linux-release/pngoptimizer" "$PODEB/usr/bin/" || HandleFail
+cp -v "$POSRC/gtk/logo128.png"            "$PODEB/usr/share/icons/hicolor/128x128/apps/pngoptimizer.png" || HandleFail
+cp -v "$POSRC/gtk/pngoptimizer.desktop"   "$PODEB/usr/share/applications/" || HandleFail
+
+SIZE=$(du -sk $PODEB | cut -f 1)
+
+cat > "$PODEB/DEBIAN/control" <<EOL
+Package: pngoptimizer
+Version: $POVER-1
+Section: base
+Priority: optional
+Architecture: amd64
+Maintainer: Hadrien Nilsson <pngoptimizer@psydk.org>
+Installed-Size: $SIZE
+Homepage: http://pngoptimizer.org
+Description: PngOptimizer
+ Optimize PNGs and convert other lossless formats to PNG
+EOL
+
+dpkg-deb --build $PODEB
+# Move the .deb in the parent directory
+mv -v $PODEB.deb ./
 
 ###############################################################################
 # PngOptimizerCL
+echo ""
+echo "========== Packaging PngOptimizerCL =========="
+make -C ../../projects/pngoptimizercl/ CONFIG=release
 
+mkdir -p linux/pngoptimizercl
 cp -v "../../projects/pngoptimizercl/linux-release/pngoptimizercl"  "linux/pngoptimizercl/" || HandleFail
 cp -v "../../projects/pngoptimizercl/Readme.txt"                    "linux/pngoptimizercl/" || HandleFail
 cp -v "../../projects/pngoptimizercl/License.txt"                   "linux/pngoptimizercl/" || HandleFail
