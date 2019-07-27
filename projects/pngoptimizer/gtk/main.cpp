3b8a2e0fa7a554fb8c92dfd4c8d50b352dde0f5e
Hadrien <hadrien@psydk.org>
2019-07-27 09:37:17 +0200

    Fix newlines

--- projects/pngoptimizer/gtk/main.cpp
+++ projects/pngoptimizer/gtk/main.cpp
@@ -1,4 +1,4 @@
-// This file is part of the PngOptimizerCL application
+// This file is part of the PngOptimizer application
 // Copyright (C) Hadrien Nilsson - psydk.org
 // For conditions of distribution and use, see copyright notice in License.txt
 /////////////////////////////////////////////////////////////////////////////////////
